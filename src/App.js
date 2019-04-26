bc9f4d7b4eb6d2fed46ec720e8b78de4201a8ab8
bogodars <bogodar@gmail.com>
2019-04-26 17:26:57 +0300

    updating edits

--- src/App.js
+++ src/App.js
@@ -97,6 +97,7 @@ class App extends Component {
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count}))
             })
+
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
