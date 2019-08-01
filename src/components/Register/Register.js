e210555fc19f1be6448fa08ceb589ca61c1e424e
bogodars <bogodar@gmail.com>
2019-08-01 17:03:35 +0300

    Changed localhost

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://calm-fortress-85241.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
